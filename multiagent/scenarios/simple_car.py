1afb6cb29fdfefba2296f876a96ccbe4caf91775
tessavdheiden <tessavdheiden@gmail.com>
2020-07-24 12:25:26 +0200

    added car

--- multiagent/scenarios/simple_car.py
+++ multiagent/scenarios/simple_car.py
@@ -17,30 +17,21 @@ class DynamicAgent(Agent):
         self.body = None
 
 
-class RoadWorld(World):
-    def __init__(self):
-        super(RoadWorld, self).__init__()
-        self.track = None
-        self.physics = None
-        self.top_view = None
-
-
 class Scenario(BaseScenario):
     def make_world(self):
         world = RoadWorld()
         # set any world properties first
         world.dim_p = 2 # x, y, orientation, speed
         world.collaborative = True
-        road = CarRacing(seed=2) # TODO: remove seed and reset track in self.reset_world
-        world.road = road
 
         # add agents
-        self.num_agents = 1
-        world.agents = [DynamicAgent() for i in range(self.num_agents)]
+        num_agents = 2
+        world.agents = [DynamicAgent() for i in range(num_agents)]
         for i, agent in enumerate(world.agents):
             agent.name = 'agent %d' % i
             agent.collide = True
             agent.silent = True
+            agent.color = colors[i]
             agent.body = Car()
             agent.size = 0.1
 
@@ -51,7 +42,8 @@ class Scenario(BaseScenario):
             landmark.name = 'landmark %d' % i
             landmark.collide = False
             landmark.movable = False
-            landmark.size = 0.01
+            landmark.size = 0.1
+            landmark.color = colors[i]
 
         world.surfaces = [Surface() for i in range(1)]
         for i, s in enumerate(world.surfaces):
@@ -60,50 +52,41 @@ class Scenario(BaseScenario):
             s.movable = False
 
         # make initial conditions
-        self.arc_lengths = None
-        world.top_view = road.render
-        world.road.reset()
         self.reset_world(world)
         return world
 
-    def scale_track(self, track, pos):
+    def scale_track(self, pos, min_x, min_y, max_x, max_y):
         x, y = pos
-        track_width = abs(max(track[:, 0]) - min(track[:, 0]))
-        track_height = abs(max(track[:, 1]) - min(track[:, 1]))
-        scale = max(track_height, track_width) / 1.8
-        x_new = (x - min(track[:, 0])) / scale - .9
-        y_new = (y - min(track[:, 1])) / scale - .9
+        track_width = abs(max_x - min_x)
+        track_height = abs(max_y - min_y)
+        scale = max(track_height, track_width) / 2
+        x_new = (x - min_x) / scale - 1
+        y_new = (y - min_y) / scale - 1
         return np.array([x_new, y_new])
 
     def reset_world(self, world):
-        track = np.array(world.road.track)[:, 2:4]
-        normalized_track = np.array([self.scale_track(track, location) for location in track])
+        coord = np.array(world.track)[:, 2:4]
+        min_x, min_y, max_x, max_y = min(coord[:, 0]),  min(coord[:, 1]),  max(coord[:, 0]),  max(coord[:, 1])
+        norm_coord = np.array([self.scale_track(c, min_x, min_y, max_x, max_y) for c in coord])
 
-        # random properties for agents
+        # equal distance between landmarks
         for i, landmark in enumerate(world.landmarks):
-            landmark.color = colors[i]
-            idx = np.minimum(int(len(normalized_track) / len(world.landmarks) * i), len(normalized_track) - 1)
-            landmark.state.p_pos = normalized_track[idx]
+            idx = np.minimum(int(len(norm_coord) / len(world.landmarks) * i), len(norm_coord) - 1)
+            landmark.state.p_pos = norm_coord[idx]
             landmark.state.p_vel = np.zeros(world.dim_p)
 
-        # want listener to go to the goal landmark
-        world.agents[0].goal = world.landmarks[-1]
-        world.agents[0].goals = np.zeros(len(world.landmarks))
-
-        #self.arc_lengths = np.linalg.norm(normalized_track - np.roll(normalized_track, -1, axis=0), axis=1)
-
+        # all agents start somewhere
         for i, agent in enumerate(world.agents):
-            agent.color = colors[i]
-            agent.action_callback = None
-            agent.state.p_pos = world.landmarks[0].state.p_pos + np.random.uniform(-.1,+.1, world.dim_p)
+            agent.state.p_pos = np.random.uniform(-1, 1, world.dim_p)
             agent.state.p_vel = np.zeros(world.dim_p)
 
+        # pure for visualizing the track
         for i, surface in enumerate(world.surfaces):
-            surface.color = np.array([c for (_, c) in world.road.road_poly])
+            surface.color = np.array([color for (_, color) in world.road_poly])
             surface.state.p_pos = np.zeros(world.dim_p)
             surface.state.p_vel = np.zeros(world.dim_p)
-            surface.v = track
-            surface.poly = np.array([[self.scale_track(track, p) for p in ps] for (ps, _) in world.road.road_poly])
+            surface.poly = np.array([[self.scale_track(c_i, min_x, min_y, max_x, max_y) for c_i in coordinates] for (coordinates, _) in world.road_poly])
+            surface.v = surface.poly[:, 1]
 
     def is_collision(self, agent1, agent2):
         delta_pos = agent1.state.p_pos - agent2.state.p_pos
@@ -160,14 +143,14 @@ class Scenario(BaseScenario):
             return False
 
     def reward(self, agent, world):
-        rew = -1 if self.outside_boundary(agent) else 0
-        # squared distance from listener to landmark
-        rew -= np.sum(np.square(agent.state.p_pos - agent.goal.state.p_pos))
+        rew = 0.
+        for l in world.landmarks:
+            dists = [np.sqrt(np.sum(np.square(a.state.p_pos - l.state.p_pos))) for a in world.agents]
+            rew -= min(dists)
+        for a in world.agents:
+            if self.outside_boundary(a):
+                rew -= 1.
         return rew
-        # img_rgb = world.top_view(mode="state_pixels")
-        # # green penalty
-        # if np.mean(img_rgb[:, :, 1]) > 185.0:
-        #     rew -= 0.05
 
     @staticmethod
     def rgb2gray(rgb, norm=True, scale_factor=2):
@@ -183,30 +166,270 @@ class Scenario(BaseScenario):
 
     def observation(self, agent, world):
         #return self.rgb2gray(world.top_view(mode="state_pixels")).reshape(-1)
+        def rotate_entity(entity):
+            dists = np.linalg.norm(entity.state.p_pos - world.surfaces[0].v, axis=1)
+            idx = np.argmin(dists)
+            entity.state.p_pos = world.surfaces[0].v[(idx+1) % len(world.surfaces[0].v)]
 
-        # goal color
-        goal_color = np.zeros(world.dim_color)
-        if agent.goal is not None:
-            goal_color = np.array([int(''.join(x for x in agent.goal.name if x.isdigit()))])
 
+        for landmark in world.landmarks:
+            rotate_entity(landmark)
+
+        # get positions of all entities in this agent's reference frame
+        other_pos = []
+        for other in world.agents:
+            if other == agent: continue
+            other_pos.append(other.state.p_pos - agent.state.p_pos)
         # get positions of all entities in this agent's reference frame
         entity_pos = []
         for entity in world.landmarks:
             entity_pos.append(entity.state.p_pos - agent.state.p_pos)
+        return np.concatenate([agent.state.p_vel] + [agent.state.p_pos] + entity_pos + other_pos)
 
-        return np.concatenate([agent.state.p_vel] + entity_pos + [goal_color])
 
     def done(self, agent, world):
-        if np.all(agent.goals):
-            return True
-
-        # define new target if target reached
-        # if self.is_collision(agent, agent.goal):
-        #     idx = int(''.join(x for x in agent.goal.name if x.isdigit()))
-        #     agent.goals[idx] = True
-        #     agent.goal = world.landmarks[idx+1]
-
-        return False # self.lat_dist(agent, world) > .01
+        pass
 
     def benchmark_data(self, agent, world):
         return (self.reward(agent, world), )
+
+
+import math
+from gym.utils import seeding
+import Box2D
+from Box2D.b2 import (edgeShape, circleShape, fixtureDef, polygonShape, revoluteJointDef, contactListener)
+
+STATE_W = 96   # less than Atari 160x192
+STATE_H = 96
+VIDEO_W = 600
+VIDEO_H = 400
+WINDOW_W = 1000
+WINDOW_H = 800
+
+SCALE = 6.0             # Track scale
+TRACK_RAD = 900/SCALE   # Track is heavily morphed circle with this radius
+PLAYFIELD = 2000/SCALE  # Game over boundary
+FPS = 50                # Frames per second
+ZOOM = 2.7              # Camera zoom
+ZOOM_FOLLOW = True      # Set to False for fixed view (don't use zoom)
+
+
+TRACK_DETAIL_STEP = 21/SCALE
+TRACK_TURN_RATE = 0.31
+TRACK_WIDTH = 40/SCALE
+BORDER = 8/SCALE
+BORDER_MIN_COUNT = 4
+
+ROAD_COLOR = [0.4, 0.4, 0.4]
+
+
+class RoadWorld(World):
+    def __init__(self):
+        super(RoadWorld, self).__init__()
+        self.track = None
+        self.road = None
+        self.road_poly = []
+        self.contactListener_keepref = FrictionDetector(self)
+        self.box2d = Box2D.b2World((0, 0), contactListener=self.contactListener_keepref)
+
+        self.fd_tile = fixtureDef(
+            shape=polygonShape(vertices=[(0, 0), (1, 0), (1, -1), (0, -1)]))
+
+        self.seed()
+        while True:
+            success = self._create_track()
+            if success:
+                break
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def _create_track(self):
+        CHECKPOINTS = 12
+
+        # Create checkpoints
+        checkpoints = []
+        for c in range(CHECKPOINTS):
+            alpha = 2 * math.pi * c / CHECKPOINTS + self.np_random.uniform(0, 2 * math.pi * 1 / CHECKPOINTS)
+            rad = self.np_random.uniform(TRACK_RAD / 3, TRACK_RAD)
+            if c == 0:
+                alpha = 0
+                rad = 1.5 * TRACK_RAD
+            if c == CHECKPOINTS - 1:
+                alpha = 2 * math.pi * c / CHECKPOINTS
+                self.start_alpha = 2 * math.pi * (-0.5) / CHECKPOINTS
+                rad = 1.5 * TRACK_RAD
+            checkpoints.append((alpha, rad * math.cos(alpha), rad * math.sin(alpha)))
+        self.road = []
+
+        # Go from one checkpoint to another to create track
+        x, y, beta = 1.5 * TRACK_RAD, 0, 0
+        dest_i = 0
+        laps = 0
+        track = []
+        no_freeze = 2500
+        visited_other_side = False
+        while True:
+            alpha = math.atan2(y, x)
+            if visited_other_side and alpha > 0:
+                laps += 1
+                visited_other_side = False
+            if alpha < 0:
+                visited_other_side = True
+                alpha += 2 * math.pi
+            while True:  # Find destination from checkpoints
+                failed = True
+                while True:
+                    dest_alpha, dest_x, dest_y = checkpoints[dest_i % len(checkpoints)]
+                    if alpha <= dest_alpha:
+                        failed = False
+                        break
+                    dest_i += 1
+                    if dest_i % len(checkpoints) == 0:
+                        break
+                if not failed:
+                    break
+                alpha -= 2 * math.pi
+                continue
+            r1x = math.cos(beta)
+            r1y = math.sin(beta)
+            p1x = -r1y
+            p1y = r1x
+            dest_dx = dest_x - x  # vector towards destination
+            dest_dy = dest_y - y
+            proj = r1x * dest_dx + r1y * dest_dy  # destination vector projected on rad
+            while beta - alpha > 1.5 * math.pi:
+                beta -= 2 * math.pi
+            while beta - alpha < -1.5 * math.pi:
+                beta += 2 * math.pi
+            prev_beta = beta
+            proj *= SCALE
+            if proj > 0.3:
+                beta -= min(TRACK_TURN_RATE, abs(0.001 * proj))
+            if proj < -0.3:
+                beta += min(TRACK_TURN_RATE, abs(0.001 * proj))
+            x += p1x * TRACK_DETAIL_STEP
+            y += p1y * TRACK_DETAIL_STEP
+            track.append((alpha, prev_beta * 0.5 + beta * 0.5, x, y))
+            if laps > 4:
+                break
+            no_freeze -= 1
+            if no_freeze == 0:
+                break
+
+        # Find closed loop range i1..i2, first loop should be ignored, second is OK
+        i1, i2 = -1, -1
+        i = len(track)
+        while True:
+            i -= 1
+            if i == 0:
+                return False  # Failed
+            pass_through_start = track[i][0] > self.start_alpha and track[i - 1][0] <= self.start_alpha
+            if pass_through_start and i2 == -1:
+                i2 = i
+            elif pass_through_start and i1 == -1:
+                i1 = i
+                break
+
+        assert i1 != -1
+        assert i2 != -1
+
+        track = track[i1:i2 - 1]
+
+        first_beta = track[0][1]
+        first_perp_x = math.cos(first_beta)
+        first_perp_y = math.sin(first_beta)
+        # Length of perpendicular jump to put together head and tail
+        well_glued_together = np.sqrt(
+            np.square(first_perp_x * (track[0][2] - track[-1][2])) +
+            np.square(first_perp_y * (track[0][3] - track[-1][3])))
+        if well_glued_together > TRACK_DETAIL_STEP:
+            return False
+
+        # Red-white border on hard turns
+        border = [False] * len(track)
+        for i in range(len(track)):
+            good = True
+            oneside = 0
+            for neg in range(BORDER_MIN_COUNT):
+                beta1 = track[i - neg - 0][1]
+                beta2 = track[i - neg - 1][1]
+                good &= abs(beta1 - beta2) > TRACK_TURN_RATE * 0.2
+                oneside += np.sign(beta1 - beta2)
+            good &= abs(oneside) == BORDER_MIN_COUNT
+            border[i] = good
+        for i in range(len(track)):
+            for neg in range(BORDER_MIN_COUNT):
+                border[i - neg] |= border[i]
+
+        # Create tiles
+        for i in range(len(track)):
+            alpha1, beta1, x1, y1 = track[i]
+            alpha2, beta2, x2, y2 = track[i - 1]
+            road1_l = (x1 - TRACK_WIDTH * math.cos(beta1), y1 - TRACK_WIDTH * math.sin(beta1))
+            road1_r = (x1 + TRACK_WIDTH * math.cos(beta1), y1 + TRACK_WIDTH * math.sin(beta1))
+            road2_l = (x2 - TRACK_WIDTH * math.cos(beta2), y2 - TRACK_WIDTH * math.sin(beta2))
+            road2_r = (x2 + TRACK_WIDTH * math.cos(beta2), y2 + TRACK_WIDTH * math.sin(beta2))
+            vertices = [road1_l, road1_r, road2_r, road2_l]
+            self.fd_tile.shape.vertices = vertices
+            t = self.box2d.CreateStaticBody(fixtures=self.fd_tile)
+            t.userData = t
+            c = 0.01 * (i % 3)
+            t.color = [ROAD_COLOR[0] + c, ROAD_COLOR[1] + c, ROAD_COLOR[2] + c]
+            t.road_visited = False
+            t.road_friction = 1.0
+            t.fixtures[0].sensor = True
+            self.road_poly.append(([road1_l, road1_r, road2_r, road2_l], t.color))
+            self.road.append(t)
+            if border[i]:
+                side = np.sign(beta2 - beta1)
+                b1_l = (x1 + side * TRACK_WIDTH * math.cos(beta1), y1 + side * TRACK_WIDTH * math.sin(beta1))
+                b1_r = (x1 + side * (TRACK_WIDTH + BORDER) * math.cos(beta1),
+                        y1 + side * (TRACK_WIDTH + BORDER) * math.sin(beta1))
+                b2_l = (x2 + side * TRACK_WIDTH * math.cos(beta2), y2 + side * TRACK_WIDTH * math.sin(beta2))
+                b2_r = (x2 + side * (TRACK_WIDTH + BORDER) * math.cos(beta2),
+                        y2 + side * (TRACK_WIDTH + BORDER) * math.sin(beta2))
+                self.road_poly.append(([b1_l, b1_r, b2_r, b2_l], (1, 1, 1) if i % 2 == 0 else (1, 0, 0)))
+        self.track = track
+        return True
+
+
+class FrictionDetector(contactListener):
+    def __init__(self, env):
+        contactListener.__init__(self)
+        self.env = env
+
+    def BeginContact(self, contact):
+        self._contact(contact, True)
+
+    def EndContact(self, contact):
+        self._contact(contact, False)
+
+    def _contact(self, contact, begin):
+        tile = None
+        obj = None
+        u1 = contact.fixtureA.body.userData
+        u2 = contact.fixtureB.body.userData
+        if u1 and "road_friction" in u1.__dict__:
+            tile = u1
+            obj = u2
+        if u2 and "road_friction" in u2.__dict__:
+            tile = u2
+            obj = u1
+        if not tile:
+            return
+
+        tile.color[0] = ROAD_COLOR[0]
+        tile.color[1] = ROAD_COLOR[1]
+        tile.color[2] = ROAD_COLOR[2]
+        if not obj or "tiles" not in obj.__dict__:
+            return
+        if begin:
+            obj.tiles.add(tile)
+            if not tile.road_visited:
+                tile.road_visited = True
+                self.env.reward += 1000.0/len(self.env.track)
+                self.env.tile_visited_count += 1
+        else:
+            obj.tiles.remove(tile)
--- multiagent/scenarios/simple_order.py
+++ multiagent/scenarios/simple_order.py
@@ -2,16 +2,22 @@ import numpy as np
 from multiagent.core import World, Agent, Landmark
 from multiagent.scenario import BaseScenario
 
+theta = np.radians(5)
+c, s = np.cos(theta), np.sin(theta)
+R = np.array(((c, -s), (s, c)))
+
+vecmod = np.vectorize(lambda x, y : (x + y / 2) % y - y / 2)
+
 class Scenario(BaseScenario):
     def make_world(self):
         world = World()
         # add agents
-        world.agents = [Agent() for i in range(1)]
+        world.agents = [Agent() for i in range(3)]
         for i, agent in enumerate(world.agents):
             agent.name = 'agent %d' % i
             agent.collide = False
             agent.silent = True
-            agent.size = .2
+            agent.size = .1
         # add landmarks
         num_land = 3
         world.landmarks = [Landmark() for i in range(num_land)]
@@ -19,10 +25,11 @@ class Scenario(BaseScenario):
             landmark.name = 'landmark %d' % i
             landmark.collide = False
             landmark.movable = False
-            landmark.size = .2
+            landmark.size = .1
 
         # make initial conditions
         self.reset_world(world)
+        self.dims = (2, 2)
         return world
 
     def reset_world(self, world):
@@ -31,32 +38,45 @@ class Scenario(BaseScenario):
         world.landmarks[1].color = np.array([0.25, 0.75, 0.25])
         world.landmarks[2].color = np.array([0.25, 0.25, 0.75])
         # set random initial states
-        for agent in world.agents:
+        for i, agent in enumerate(world.agents):
             agent.state.p_pos = np.random.uniform(-1,+1, world.dim_p)
             agent.state.p_vel = np.zeros(world.dim_p)
             agent.state.c = np.zeros(world.dim_c)
+            agent.color = world.landmarks[i].color
         for i, landmark in enumerate(world.landmarks):
-            landmark.state.p_pos = np.random.uniform(-1,+1, world.dim_p)
+            theta = np.random.uniform(0, np.pi)
+            landmark.state.p_pos = .5 * np.array([np.cos(theta), np.sin(theta)])
             landmark.state.p_vel = np.zeros(world.dim_p)
-        world.agents[0].goal = world.landmarks[0]
-        world.agents[0].goals = np.zeros(len(world.landmarks))
-        # random properties for agents
-        world.agents[0].color = world.agents[0].goal.color
 
     def reward(self, agent, world):
-        rew = - len(world.landmarks) + sum(world.agents[0].goals)
-        rew -= np.sum(np.square(agent.state.p_pos - agent.goal.state.p_pos))
-        if self.outside_boundary(agent):
-            rew -= 1.
+        rew = 0.
+        for l in world.landmarks:
+            dists = [np.sqrt(np.sum(np.square(a.state.p_pos - l.state.p_pos))) for a in world.agents]
+            rew -= min(dists)
+        for a in world.agents:
+            if self.outside_boundary(a):
+                rew -= 1.
         return rew
 
     def observation(self, agent, world):
+        def rotate_entity(entity):
+            entity.state.p_pos = np.dot(R, entity.state.p_pos) + np.random.uniform(-.01, .01, world.dim_p)
+            if self.dims:
+                entity.state.p_pos = vecmod(entity.state.p_pos, self.dims)
+
+        for landmark in world.landmarks:
+            rotate_entity(landmark)
+
+        # get positions of all entities in this agent's reference frame
+        other_pos = []
+        for other in world.agents:
+            if other == agent: continue
+            other_pos.append(other.state.p_pos - agent.state.p_pos)
         # get positions of all entities in this agent's reference frame
         entity_pos = []
         for entity in world.landmarks:
             entity_pos.append(entity.state.p_pos - agent.state.p_pos)
-
-        return np.concatenate([agent.state.p_vel] + entity_pos + [agent.goal.color])
+        return np.concatenate([agent.state.p_vel] + [agent.state.p_pos] + entity_pos + other_pos)
 
     def outside_boundary(self, agent):
         if agent.state.p_pos[0] > 1 or agent.state.p_pos[0] < -1 or agent.state.p_pos[1] > 1 or agent.state.p_pos[1] < -1:
@@ -71,16 +91,7 @@ class Scenario(BaseScenario):
         return True if dist < dist_min else False
 
     def done(self, agent, world):
-        if np.all(agent.goals == 1):
-            return True
-
-        # define new target if target reached
-        if self.is_collision(agent, agent.goal):
-            idx = int(''.join(x for x in agent.goal.name if x.isdigit()))
-            agent.goals[idx] = 1
-            agent.goal = world.landmarks[idx+1] if idx < len(world.landmarks) - 1 else world.landmarks[idx]
-
-        return False
+        pass
 
     def benchmark_data(self, agent, world):
         return (self.reward(agent, world), )
