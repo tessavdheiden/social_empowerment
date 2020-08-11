bb206576be32a269a74f9300ea794021fc6fa8bd
Tessa van der Heiden <tessavdheiden@gmail.com>
2020-08-11 12:20:05 +0200

    added car with obstacles

--- README.md
+++ README.md
@@ -31,21 +31,31 @@ tensorboard --logdir models/model_name
 ## Simulation Videos
 
 ### Cooperative Communication
+The moving agent needs to go to a landmark with a particular color. However, it is blind and another agent sends messages
+that help to navigate. Since there are more landmarks than communication channels, the speaking
+agent cannot simply output a symbol corresponding to a particular color. If the listening agent is not receptive to
+the messages, the speaker will output random signals. This in turn forces the listener to ignore them. With *empowerment* 
+agents remain reactive to one another. 
+ 
 DDPG              | MADDPG             | EMADDPG
 :-------------------------:|:-------------------------:|:-------------------------:
 <img src="https://user-images.githubusercontent.com/24938569/89288011-3bad5180-d655-11ea-8c5d-d3c895510985.gif" width="200" />|<img src="https://user-images.githubusercontent.com/24938569/89042716-abff5e80-d347-11ea-9ff1-fed829d10d57.gif" width="200" />|<img src="https://user-images.githubusercontent.com/24938569/89042658-91c58080-d347-11ea-8acc-92f1ef9a7b15.gif" width="200" />
 
 ### Cooperative Coordination
+In this simple task agents need to cover all landmarks. MADDPG algorithm is trained by self-play, causing them to agree upon
+a rule. For example, agent 1 goes to the red, agent 2 goes to the green and agent 3 to the blue landmark. 
+At test time, agent 1 is paired with agent 2 and 3 from a different run and so the former rule does not necessarily results
+in the most efficient landmark selection. In contrast, EMADDPG uses *empowerment* that results in picking a landmark closest
+to each agent.  
+
 MADDPG             | EMADDPG
 :-------------------------:|:-------------------------:
 <img src="https://user-images.githubusercontent.com/24938569/89157902-94092400-d56d-11ea-985e-ec243e9daa49.gif" width="200" />|<img src="https://user-images.githubusercontent.com/24938569/89157957-a84d2100-d56d-11ea-93ec-a3dd27494d24.gif" width="200" />
 
 
 ### Cooperative Driving
-
-DDPG                | MADDPG             
-:-------------------------:|:-------------------------:
-<img src="https://user-images.githubusercontent.com/24938569/89404721-bdb37e00-d71a-11ea-93f8-b64af5de5bec.gif" width="200" />|<img src="https://user-images.githubusercontent.com/24938569/89404866-f6535780-d71a-11ea-8d25-00172886a9d8.gif" width="200" />
+Cars need to stay on the road and need to avoid collisions. Agents only obtain a small top view image and their own states, 
+such as orientation and velocity.
 
 Visual inputs:
 
@@ -53,6 +63,10 @@ Red Agent          | Green Agent
 :-------------------------:|:-------------------------:
 <img src="https://user-images.githubusercontent.com/24938569/89423810-404a3680-d737-11ea-90a4-eb770f49d2cd.gif" width="40" /> | <img src="https://user-images.githubusercontent.com/24938569/89423739-2e689380-d737-11ea-91c0-3bbc108085e3.gif" width="40" />
 
+|                          |DDPG                | MADDPG             
+:------------------------- |:-------------------------:|:-------------------------:
+Overtaking |<img src="https://user-images.githubusercontent.com/24938569/89404721-bdb37e00-d71a-11ea-93f8-b64af5de5bec.gif" width="200" />|<img src="https://user-images.githubusercontent.com/24938569/89404866-f6535780-d71a-11ea-8d25-00172886a9d8.gif" width="200" />
+Obstacle avoidance |<img src="https://user-images.githubusercontent.com/24938569/89886358-cbf71380-dbcc-11ea-8467-25ae2d6a7e78.gif" width="200" />|<img src="https://user-images.githubusercontent.com/24938569/89886414-e16c3d80-dbcc-11ea-9924-f1be94b94d06.gif" width="200" />
 ### Cooperative Coordination
 
 | Agent     | Average dist. | Collisions % |
---
+++ multiagent/scenarios/simple_car2.py
@@ -0,0 +1,148 @@
+import numpy as np
+from multiagent.core import Surface, Landmark
+from multiagent.dynamic_agent import DynamicAgent
+from multiagent.road_world import RoadWorld, ROAD_COLOR, TRACK_RAD
+from multiagent.scenario import BaseScenario
+from multiagent.scenarios.car_dynamics import Car, HULL_POLY1, HULL_POLY2, HULL_POLY3, HULL_POLY4, SIZE
+
+
+colors = np.array([[0.65, 0.15, 0.15], [0.15, 0.65, 0.15], [0.15, 0.15, 0.65],
+                   [0.15, 0.65, 0.65], [0.65, 0.15, 0.65], [0.65, 0.65, 0.15]])
+
+SCALE = TRACK_RAD * 2 / 2
+
+
+class Scenario(BaseScenario):
+    def make_world(self):
+        world = RoadWorld()
+        # set any world properties first
+        world.dim_p = 2 # x, y, orientation, speed
+        num_obstacles = 3
+        world.collaborative = True
+
+        # add agents
+        num_agents = 2
+        world.set_agents([DynamicAgent() for i in range(num_agents)])
+        for i, agent in enumerate(world.agents):
+            agent.name = 'dynamic agent %d' % i
+            agent.collide = True
+            agent.silent = True
+            agent.color = colors[i]
+            agent.body = Car(world.box2d)
+            agent.scale = SCALE
+
+            agent.shape = [[(x * SIZE / SCALE, y * SIZE / SCALE) for x, y in HULL_POLY1],
+                           [(x * SIZE / SCALE, y * SIZE / SCALE) for x, y in HULL_POLY2],
+                           [(x * SIZE / SCALE, y * SIZE / SCALE) for x, y in HULL_POLY3],
+                           [(x * SIZE / SCALE, y * SIZE / SCALE) for x, y in HULL_POLY4]]
+            agent.size = SIZE
+
+        world.agents[0].max_speed = .1
+        world.agents[1].max_speed = .15
+
+        world.surfaces = [Surface() for i in range(1)]
+        for i, s in enumerate(world.surfaces):
+            s.name = 'surface %d' % i
+            s.collide = False
+            s.movable = False
+
+        # add obstacles
+        world.obstacles = [Landmark() for i in range(num_obstacles)]
+        for i, landmark in enumerate(world.obstacles):
+            landmark.name = 'obstacle %d' % i
+            landmark.collide = True
+            landmark.movable = False
+            landmark.size = 0.01
+            landmark.color = np.array([0.255, 0.165, 0.0])
+
+        # make initial conditions
+        self.reset_world(world)
+        return world
+
+    def reset_world(self, world):
+        world.reset()
+        coord = np.array(world.track)[:, 2:4]
+        norm_coord = np.array([(c[0] / SCALE, c[1] / SCALE) for c in coord])
+
+        # all agents start somewhere
+        start_i = np.random.choice(len(coord))
+        dist = 5
+        for i, agent in enumerate(world.agents):
+            idx = (start_i - i * dist) % len(coord)
+            agent.state.p_pos = norm_coord[idx]
+            agent.state.p_vel = np.zeros(world.dim_p)
+            agent.state.angle = world.track[idx][1]
+            agent.body.make(*world.track[idx][1:4]) # TODO: set x, y
+
+        # pure for visualizing the track
+        for i, surface in enumerate(world.surfaces):
+            surface.color = np.array([color for (_, color) in world.road_poly])
+            surface.state.p_pos = np.zeros(world.dim_p)
+            surface.state.p_vel = np.zeros(world.dim_p)
+            surface.poly = np.array([[(c_i[0] / SCALE, c_i[1] / SCALE) for c_i in coordinates] for (coordinates, _) in world.road_poly])
+            surface.v = np.mean(surface.poly[:, 0:2], axis=1)
+
+        # reset obstacle position
+        for i, landmark in enumerate(world.obstacles):
+            idx = (start_i + (i+1) * dist) % len(coord)
+            landmark.state.p_pos = norm_coord[idx]
+            landmark.state.p_vel = np.zeros(world.dim_p)
+
+    def is_collision(self, agent1, agent2):
+        delta_pos = agent1.state.p_pos - agent2.state.p_pos
+        dist = np.sqrt(np.sum(np.square(delta_pos)))
+        dist_min = agent1.size + agent2.size
+        return True if dist < dist_min else False
+
+    def reward(self, agent, world):
+        rew = 0.
+        for view in world.top_views:
+            for road_color, road_patch in zip(ROAD_COLOR, view.transpose(2, 1, 0)):
+                rew -= abs(road_color - road_patch / 255.)
+        c, w, h = view.shape
+        rew /= (c*w*h)
+        rew = np.sum(rew)
+        if agent.collide:
+            for a in world.agents:
+                if self.is_collision(a, agent):
+                    rew -= 1
+            for obstacle in world.obstacles:
+                if self.is_collision(obstacle, agent):
+                    rew -= 1
+        return rew
+
+    @staticmethod
+    def rgb2gray(rgb, norm=True):
+        # rgb image -> gray [0, 1]
+        gray = np.dot(rgb[..., :], [0.299, 0.587, 0.114])
+        if norm:
+            # normalize
+            gray = gray / 128. - 1.
+        return gray
+
+    def observation(self, agent, world):
+        # get positions of all entities in this agent's reference frame
+        other_pos = []
+        view = None
+        for i, other in enumerate(world.agents):
+            if other == agent:
+                view = world.get_views()[i]
+            else:
+                other_pos.append(other.state.p_pos - agent.state.p_pos)
+
+        # get positions of all entities in this agent's reference frame
+        entity_pos = []
+        for entity in world.obstacles:
+            entity_pos.append(entity.state.p_pos - agent.state.p_pos)
+
+        dir = np.array([np.cos(agent.state.angle), np.sin(agent.state.angle)])
+        view = self.rgb2gray(view)
+        obs = np.concatenate([agent.state.p_vel] + [agent.state.p_pos] + [dir] + other_pos + entity_pos)
+        return np.hstack((obs, view.reshape(-1)))
+
+    def done(self, agent, world):
+        pass
+
+    def benchmark_data(self, agent, world):
+        return (self.reward(agent, world), )
+
