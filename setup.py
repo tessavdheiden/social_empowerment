f8c25243dd1010573e448b8676692f7e51b4e53c
tessavdheiden <tessavdheiden@gmail.com>
2020-09-18 18:24:46 +0200

    correct packages

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    install_requires=[
+        'gym==0.17.2',
+        'matplotlib==3.3.2',
+        'numpy==1.19.2',
+        'torch==1.5.1',
+        'torchvision==0.6.1',
+        'Box2D',
+        'pyglet==1.3.2',
+        'tensorboardX'
+    ],
+)
