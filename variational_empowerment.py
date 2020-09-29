3d000538a2d0e765f05063a0cc7ca62f50d7cdf2
tessavdheiden <tessavdheiden@gmail.com>
2020-09-29 14:09:53 +0200

    ve with source and planning taking only single agent actions to prevent one hot encoding for more agents

--- variational_empowerment.py
+++ variational_empowerment.py
@@ -17,26 +17,39 @@ class VariationalJointEmpowerment(object):
         self.planning = MLPNetwork(init_params['num_in_plan'], init_params['num_out_plan'], recurrent=True)
 
         self.transition_optimizer = Adam(self.transition.parameters(), lr=lr)
-        self.source_optimizer = Adam(list(self.source.parameters()) + list(self.planning.parameters()), lr=lr)
+        self.planning_optimizer = Adam(self.planning.parameters(), lr=lr)
+        self.source_optimizer = Adam(self.source.parameters(), lr=lr)
         self.trans_dev = 'cpu'  # device for transition
         self.source_dev = 'cpu'
         self.plan_dev = 'cpu'
 
         self.niter = 0
 
-    def compute(self, rewards, obs):
+    def compute(self, rewards, next_obs):
         with torch.no_grad():
-            obs_torch = [Variable(torch.Tensor(np.vstack(obs[:, i])),
+            next_obs = [Variable(torch.Tensor(np.vstack(next_obs[:, i])),
                       requires_grad=False) for i in range(rewards.shape[1])]
-            obs_torch = torch.cat(obs_torch, dim=1)
-            acs = self.source(obs_torch)
-            next_obs_trans = self.transition(torch.cat((obs_torch, onehot_from_logits(acs)), dim=1))
-            plan_in = torch.cat((obs_torch, next_obs_trans), dim=1)
-            acs_plan = self.planning(plan_in)
-            ac = onehot_from_logits(acs)
-            emp = gumbel_softmax(acs_plan, device=self.source_dev) * ac - \
-                  gumbel_softmax(acs, device=self.source_dev) * ac
-            i_rews = emp.mean() * torch.ones((1, rewards.shape[1]))
+
+            acs_src = []
+            prob_src = []
+            for no in next_obs:
+                acs_src.append(gumbel_softmax(self.source(no), device=self.source_dev, hard=True))
+                prob_src.append(gumbel_softmax(self.source(no), device=self.source_dev, hard=False))
+
+            trans_in = torch.cat((*next_obs, *acs_src), dim=1)
+            trans_out = self.transition(trans_in)
+            prob_plan = []
+            n_obs = len(next_obs[0][0])
+            for i, no in enumerate(next_obs):
+                nno = trans_out[:, i * n_obs:(i + 1) * n_obs]
+                plan_in = torch.cat((no, nno), dim=1)
+                prob_plan.append(gumbel_softmax(self.planning(plan_in), device=self.plan_dev, hard=False))
+            prob_plan = torch.cat(prob_plan, dim=1)
+            prob_src = torch.cat(prob_src, dim=1)
+            acs_src = torch.cat(acs_src, dim=1)
+
+            E = acs_src * prob_plan - acs_src * prob_src
+            i_rews = E.mean() * torch.ones((1, rewards.shape[1]))
             return i_rews.numpy()
 
     def update(self, sample, logger=None):
@@ -49,27 +62,43 @@ class VariationalJointEmpowerment(object):
         trans_loss.backward()
         self.transition_optimizer.step()
 
+        self.planning_optimizer.zero_grad()
+        acs_plan = []
+        for o, no in zip(obs, next_obs):
+            plan_in = torch.cat((o, no), dim=1)
+            acs_plan.append(gumbel_softmax(self.planning(plan_in), device=self.plan_dev, hard=True))
+        acs_plan = torch.cat(acs_plan, dim=1)
+        acs_torch = torch.cat(acs, dim=1)
+        plan_loss = MSELoss(acs_plan, acs_torch)
+        plan_loss.backward()
+        self.planning_optimizer.step()
+
         self.source_optimizer.zero_grad()
-        obs_torch = torch.cat(next_obs, dim=1)
-        acs_src = self.source(obs_torch)
+        acs_src = []
+        prob_src = []
+        for no in next_obs:
+            acs_src.append(gumbel_softmax(self.source(no), device=self.source_dev, hard=True))
+            prob_src.append(gumbel_softmax(self.source(no), device=self.source_dev, hard=False))
         with torch.no_grad():
-            trans_in = torch.cat((obs_torch, onehot_from_logits(acs_src)), dim=1)
-            next_obs_trans = self.transition(trans_in)
-        plan_in = torch.cat((obs_torch, next_obs_trans), dim=1)
-        acs_plan = self.planning(plan_in)
+            trans_in = torch.cat((*next_obs, *acs_src), dim=1)
+            trans_out = self.transition(trans_in)
+        prob_plan = []
+        n_obs = len(next_obs[0][0])
+        for i, no in enumerate(next_obs):
+            nno = trans_out[:, i*n_obs:(i+1)*n_obs]
+            plan_in = torch.cat((no, nno), dim=1)
+            prob_plan.append(gumbel_softmax(self.planning(plan_in), device=self.plan_dev, hard=False))
+        prob_plan = torch.cat(prob_plan, dim=1)
+        prob_src = torch.cat(prob_src, dim=1)
+        acs_src = torch.cat(acs_src, dim=1)
 
-        ac = onehot_from_logits(acs_src)
-        emp = - gumbel_softmax(acs_plan, device=self.source_dev) * ac + gumbel_softmax(acs_src, device=self.source_dev) * ac
-
-        i_rews = emp.mean()
+        E = acs_src * prob_plan - acs_src * prob_src
+        i_rews = -E.mean()
         i_rews.backward()
         self.source_optimizer.step()
 
-        if logger is not None:
-            logger.add_scalars('empowerment',
-                               {'i_rews': i_rews,
-                                'trans_loss': trans_loss},
-                               self.niter)
+        print(f'transition loss = {trans_loss.detach():.3f} planning loss = {plan_loss.detach():.3f} E = {E.mean().detach():.3f}')
+
         self.niter += 1
 
     def prep_training(self, device='gpu'):
@@ -116,11 +145,11 @@ class VariationalJointEmpowerment(object):
             num_out_planning = num_in_transition = num_out_transition = 0
         for acsp, obsp in zip(env.action_space, env.observation_space):
 
-            num_in_source += obsp.shape[0]
-            num_out_source += acsp.n
+            num_in_source = obsp.shape[0]
+            num_out_source = acsp.n
 
-            num_in_planning += 2*obsp.shape[0]
-            num_out_planning += acsp.n
+            num_in_planning = 2*obsp.shape[0]
+            num_out_planning = acsp.n
 
             num_in_transition += obsp.shape[0] + acsp.n
             num_out_transition += obsp.shape[0]
